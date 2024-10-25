7eaf0c86cb58c3e2562e9753691fd724914a4da4
harshoberoi <121999815+harshoberoi@users.noreply.github.com>
2024-10-25 10:30:30 +0530

    Accepted

---
+++ LeetCode/Problems/LC 017XX Series/1773. Count Items Matching a Rule.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int count=0;
+        int index = 0;
+
+        if(ruleKey == "color")
+        index=1;
+        else if(ruleKey=="name")
+        index=2;
+
+        for(int i = 0;i<items.size();i++)
+        {
+            if(items[i][index]==ruleValue)
+            count++;
+        }
+        return count;
+    }
+};
