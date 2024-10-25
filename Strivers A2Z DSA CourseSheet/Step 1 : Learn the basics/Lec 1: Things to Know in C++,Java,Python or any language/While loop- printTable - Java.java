08b5541a41af1afe3f986ff8b0caf4e82c2552bb
harshoberoi <121999815+harshoberoi@users.noreply.github.com>
2024-10-25 13:57:06 +0530

    gfg

---
+++ Strivers A2Z DSA CourseSheet/Step 1 : Learn the basics/Lec 1: Things to Know in C++,Java,Python or any language/While loop- printTable - Java.java
@@ -0,0 +1,18 @@
+
+
+//User function Template for Java
+class Geeks {
+	 static void printTable (int n) 
+	 {
+            int multiplier=10;
+            while(multiplier>0)
+            {
+                 System.out.print(multiplier*n);
+                  System.out.print(" ");
+                  multiplier--;
+                //Your code here
+            }
+       System.out.println();
+	 }
+
+}
