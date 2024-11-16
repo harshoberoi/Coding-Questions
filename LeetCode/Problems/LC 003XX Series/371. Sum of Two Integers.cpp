6c1e2bc03e4cd0997f8ec4f68cd87beceb31b99d
harshoberoi <121999815+harshoberoi@users.noreply.github.com>
2024-11-16 19:15:46 +0530

    Update 371. Sum of Two Integers.cpp

--- LeetCode/Problems/LC 003XX Series/371. Sum of Two Integers.cpp
+++ LeetCode/Problems/LC 003XX Series/371. Sum of Two Integers.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+      //  return a+b;
+
+         while(b>0){ 
+        a++;
+        b--;
+    }
+    while(b<0){ 
+        a--;
+        b++;
+    }
+    return a;
     }
 };
